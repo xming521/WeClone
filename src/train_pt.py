353b2b7415aa2526eda29bede05a256a7bd2fab1
xming521 <1223398803@qq.com>
2024-04-27 21:50:37 +0800

    统一配置文件

--- src/train_pt.py
+++ src/train_pt.py
@@ -1,5 +1,5 @@
 from llmtuner import run_exp
-from src.utils.config import load_config
+from utils.config import load_config
 
 config = load_config('train_pt')
 run_exp(config)
--- src/train_sft.py
+++ src/train_sft.py
@@ -1,6 +1,6 @@
 from llmtuner import run_exp
 from template import template_register
-from src.utils.config import load_config
+from utils.config import load_config
 
 config = load_config(arg_type='train_sft')
 
